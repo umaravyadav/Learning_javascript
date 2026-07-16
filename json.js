1e2c3ff1fb3f01fff51fe625c68366e9ac08876d
umaravyadav <umaravyadav51@gmail.com>
2026-07-16 21:31:20 +0530

    json talkk

---
+++ json.js
@@ -0,0 +1,23 @@
+// dstracture json data
+
+const jsonData = {
+    course: "JavaScript",
+    topics: ["Variables", "Functions", "Objects", "Arrays"],
+    duration: "3 months",
+    instructor: "Umarav"
+  }
+  //jsonData.courseInstructor 
+
+//   const { courseInstructor:instructor } = jsonData
+// console.log(instructor);
+
+// {
+//     "Name": "Umarav",
+//     "age": 22,
+//     "price":    "Free"
+// }
+
+// [
+//     {},
+//     {}
+// ]
