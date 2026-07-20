c6f245b91236b73080979a761458659a2fc424cb
umaravyadav <umaravyadav51@gmail.com>
2026-07-20 21:57:19 +0530

    function basic

---
+++ function.js
@@ -0,0 +1,25 @@
+// function calculateSum(a, b) {
+//     return a + b;
+// }
+// calculateSum(5, 10); // Returns 15
+
+
+// rest parameter allows us to represent an indefinite number of arguments as an array.
+// function calculateSum(... numbers) {
+//     return numbers 
+// }   
+
+// console.log(calculateSum(5, 10, 15)); // Returns [5, 10, 15]
+
+
+// const user = {
+//     name: 'John Doe',
+//     age: 30,  
+//   }
+
+//   function handObject(anyobject) {
+//     console.log(`Username is ${anyobject.name} and age is ${anyobject.age}`);
+//   }
+
+// handObject(user); // Returns "Username is John Doe and age is 30"
+
